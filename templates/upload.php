0295b38c373e3e125bece12592a3843e8a447e3c
Diwesh Ojha <dojha@sonwil.com>
2019-01-25 12:14:28 -0500

    added php tp upload image

--- templates/upload.php
+++ templates/upload.php
@@ -12,15 +12,7 @@
       if(in_array($file_ext,$extensions)=== false){
          $errors[]="extension not allowed, please choose a JPEG or PNG file.";
       }
-
-      if($file_size > 2097152){
-         $errors[]='File size must be excately 2 MB';
-      }
-
-      if(empty($errors)==true){
-         move_uploaded_file($file_tmp,"images/".$file_name);
-         echo "Success";
-      }else{
+      else{
          print_r($errors);
       }
    }
