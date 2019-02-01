f5f9ca1a8c10e8fa55dc20ead9ab23394ea4d68e
Diwesh Ojha <dojha@sonwil.com>
2019-02-01 10:35:18 -0500

    using google vision

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
+from google.cloud import vision
+from flask import Flask, render_template, redirect, url_for, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+import os
+import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'I have a dream'
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
+
+@app.route('/scan/<filename>', methods=['GET', 'POST'])
+def open_file(filename):
+    image_url = requests.args.get('url')
+    #path = requests.get(file_url)
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(image_url, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    #print('Texts:')
+
+    return render_template('browser.html', file_url=file_url, file_text=texts)
+
+
+app. run()
