b7dc4acf61639b8b40120a1d81ff374fca04f465
Diwesh Ojha <dojha@sonwil.com>
2019-02-01 16:06:38 -0500

    using google vision

--- main.py
+++ main.py
@@ -14,16 +14,35 @@ app = Flask(__name__)
 def open_file(filename):
     image_url = requests.args.get('url')
     #path = requests.get(file_url)
-    client = vision.ImageAnnotatorClient()
+    with open(image_url, 'rb') as image_file:
+        content = base64.b64encode(image_file.read())
+        content = content.decode('utf-8')
 
-    with io.open(image_url, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
-
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    #print('Texts:')
+    api_key = "AIzaSyDn1BzcyAz1Yh0_H4thvEifVuVSjYO2_64"
+    url = "https://vision.googleapis.com/v1/images:annotate?key=" + api_key
+    headers = { 'Content-Type': 'application/json' }
+    request_json = {
+        'requests': [
+            {
+                'image': {
+                    'content': content
+                },
+                'features': [
+                    {
+                        'type': "TEXT_DETECTION",
+                        'maxResults': 10
+                    }
+                ]
+            }
+        ]
+    }
+    response = requests.post(
+        url,
+        json.dumps(request_json),
+        headers
+    )
+    result = response.json()
+    texts= (result['responses'][0]['textAnnotations'][0]['description'])
 
     return render_template('browser.html', file_url=file_url, file_text=texts)
 
