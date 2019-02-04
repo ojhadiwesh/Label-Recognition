81bceda5351e0153690d044f13ef430192d7a6cf
Diwesh Ojha <dojha@sonwil.com>
2019-02-04 11:15:57 -0500

    variation of vision

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
-from google.cloud import vision
-from google.cloud.vision import types
 from flask import Flask, render_template, redirect, url_for, request
+from googleapiclient import discovery
+from oauth2client import client
+from oauth2client.contrib import appengine
+from google.appengine.api import memcache
+import argparse
+import base64
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import GoogleCredentials
 
 import os
 import time
@@ -10,14 +18,35 @@ app = Flask(__name__)
 
 @app.route('/scan', methods=['GET', 'POST'])
 def open_file(filename):
-    file_url = request.args.get('url')
-    client = vision.ImageAnnotatorClient()
-    image = vision.types.Image()
-    image.source.image_uri = file_url
-    resp = client.text_detection(image=image)
-    print('\n'.join([d.description for d in resp.text_annotations]))
+    API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+ http = httplib2.Http()
 
-    return render_template('browser.html', file_url=file_url, file_text=resp.text_annotations)
+ credentials = GoogleCredentials.get_application_default().create_scoped(
+     ['https://www.googleapis.com/auth/cloud-platform'])
+ credentials.authorize(http)
+
+ service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
+
+ with open(photo_file, 'rb') as image:
+   image_content = base64.b64encode(image.read())
+   service_request = service.images().annotate(
+     body={
+       'requests': [{
+         'image': {
+           'content': image_content
+          },
+         'features': [{
+           'type': 'LABEL_DETECTION',
+           'maxResults': 5,
+          }]
+        }]
+     })
+   response = service_request.execute()
+   for results in response['responses']:
+     if 'labelAnnotations' in results:
+       for annotations in results['labelAnnotations']:
+         print('Found label %s, score = %s' % (annotations['description'],annotations['score']))
+    return render_template('browser.html', file_url=file_url, file_text=results['labelAnnotations'])
 
 
 app. run()
