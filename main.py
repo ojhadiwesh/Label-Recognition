0d266a543a0ad63f8cb980034d75d83b9f28cdef
Diwesh Ojha <dojha@sonwil.com>
2019-02-04 11:23:55 -0500

    variation of vision

--- main.py
+++ main.py
@@ -19,33 +19,33 @@ app = Flask(__name__)
 @app.route('/scan', methods=['GET', 'POST'])
 def open_file(filename):
     API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
- http = httplib2.Http()
+    http = httplib2.Http()
 
- credentials = GoogleCredentials.get_application_default().create_scoped(
-     ['https://www.googleapis.com/auth/cloud-platform'])
- credentials.authorize(http)
+    credentials = GoogleCredentials.get_application_default().create_scoped(
+        ['https://www.googleapis.com/auth/cloud-platform'])
+    credentials.authorize(http)
 
- service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
+    service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
 
- with open(photo_file, 'rb') as image:
-   image_content = base64.b64encode(image.read())
-   service_request = service.images().annotate(
-     body={
-       'requests': [{
-         'image': {
-           'content': image_content
-          },
-         'features': [{
-           'type': 'LABEL_DETECTION',
-           'maxResults': 5,
-          }]
-        }]
-     })
-   response = service_request.execute()
-   for results in response['responses']:
-     if 'labelAnnotations' in results:
-       for annotations in results['labelAnnotations']:
-         print('Found label %s, score = %s' % (annotations['description'],annotations['score']))
+    with open(photo_file, 'rb') as image:
+        image_content = base64.b64encode(image.read())
+        service_request = service.images().annotate(
+        body={
+            'requests': [{
+                'image': {
+                'content': image_content
+                },
+                'features': [{
+                    'type': 'LABEL_DETECTION',
+                    'maxResults': 5,
+                }]
+            }]
+        })
+    response = service_request.execute()
+    for results in response['responses']:
+        if 'labelAnnotations' in results:
+            for annotations in results['labelAnnotations']:
+                print('Found label %s, score = %s' % (annotations['description'],annotations['score']))
     return render_template('browser.html', file_url=file_url, file_text=results['labelAnnotations'])
 
 
