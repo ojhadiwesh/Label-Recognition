08ebc1688ab9aea6039ca6b300f32521ba2f419f
Diwesh Ojha <dojha@sonwil.com>
2019-02-06 16:04:26 -0500

    updates to main and app.yaml

--- main.py
+++ main.py
@@ -52,19 +52,19 @@ def open_file():
     def get_document_bounds(response, feature):
         for i,page in enumerate(document.pages):
             for block in page.blocks:
-                if feature==FeatureType.BLOCK:
+                if feature=='FeatureType.BLOCK':
                     bounds.append(block.bounding_box)
                 for paragraph in block.paragraphs:
-                    if feature==FeatureType.PARA:
+                    if feature=='FeatureType.PARA':
                         bounds.append(paragraph.bounding_box)
                     for word in paragraph.words:
                         for symbol in word.symbols:
-                            if (feature == FeatureType.SYMBOL):
+                            if (feature == 'FeatureType.SYMBOL'):
                                 bounds.append(symbol.bounding_box)
-                        if (feature == FeatureType.WORD):
+                        if (feature == 'FeatureType.WORD'):
                             bounds.append(word.bounding_box)
         return bounds
-    bounds=get_document_bounds(response, FeatureType.WORD)
+    bounds=get_document_bounds(response, 'FeatureType.WORD')
     image= draw_boxes(image,bounds, 'yellow')
     def assemble_word(word):
         assembled_word=""
