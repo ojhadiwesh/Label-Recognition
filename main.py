334c19d9defdbca02126c100c1dc8edbff5ad658
Diwesh Ojha <dojha@sonwil.com>
2019-02-06 13:51:14 -0500

    updates to main and app.yaml

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from google.cloud import datastore
 from google.cloud import storage
 from google.cloud import vision
-import argparse
-import base64
-import httplib2
 
-from apiclient.discovery import build
-from oauth2client.client import GoogleCredentials
 
 import os
 import time
@@ -17,36 +12,50 @@ app = Flask(__name__)
 
 @app.route('/scan', methods=['POST'])
 def open_file():
+
     image_file = request.files['file']
-    img_name= image_file.filename
-    fileDir = os.path.dirname(os.path.realpath('__file__'))
-    UPLOAD_FOLDER= os.path.join(fileDir, '../static')
-    UPLOAD_FOLDER= os.path.abspath(os.path.realpath(UPLOAD_FOLDER))
-    image_file.save(os.path.join(UPLOAD_FOLDER, img_name))
-    API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
-    http = httplib2.Http()
+     # Create a Cloud Storage client.
+    storage_client = storage.Client()
 
-    credentials = GoogleCredentials.get_application_default().create_scoped(
-        ['https://www.googleapis.com/auth/cloud-platform'])
-    credentials.authorize(http)
+    # Get the bucket that the file will be uploaded to.
+    bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
+     # Create a new blob and upload the file's content.
+    blob = bucket.blob(photo.filename)
+    blob.upload_from_string(
+            photo.read(), content_type=photo.content_type)
 
-    service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
+    # Make the blob publicly viewable.
+    blob.make_public()
+     # Create a Cloud Vision client.
+    vision_client = vision.ImageAnnotatorClient()
 
-    with open(img_name, 'rb') as image:
-        image_content = base64.b64encode(image.read())
-        service_request = service.images().annotate(
-        body={
-            'requests': [{
-                'image': {
-                'content': image_content
-                },
-                'features': [{
-                    'type': 'TEXT_DETECTION'
-                }]
-            }]
-        })
-    response = service_request.execute()
-    for results in response['responses']:
-        if 'text_annotations' in results:
-            for annotations in results['text_annotations']:
-                return('Found label %s ' % (annotations['description']))
+    # Use the Cloud Vision client to detect a face for our image.
+    source_uri = 'gs://{}/{}'.format(CLOUD_STORAGE_BUCKET, blob.name)
+    image = vision.types.Image(
+        source=vision.types.ImageSource(gcs_image_uri=source_uri))
+    labels = vision_client.text_detection(image).text_annotations
+    # Create a Cloud Datastore client.
+    datastore_client = datastore.Client()
+
+    # Fetch the current date / time.
+    current_datetime = datetime.now()
+
+    # The kind for the new entity.
+    kind = 'labels'
+
+    # The name/ID for the new entity.
+    name = blob.name
+
+    # Create the Cloud Datastore key for the new entity.
+    key = datastore_client.key(kind, name)
+     # Construct the new entity using the key. Set dictionary values for entity
+    # keys blob_name, storage_public_url, timestamp, and text.
+    entity = datastore.Entity(key)
+    entity['blob_name'] = blob.name
+    entity['image_public_url'] = blob.public_url
+    entity['timestamp'] = current_datetime
+    entity['text'] = labels
+
+    # Save the new entity to Datastore.
+    datastore_client.put(entity)
+    return labels
