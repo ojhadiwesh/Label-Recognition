5dbadb3586029172e698e45c62e6e1510d5b9ee1
Diwesh Ojha <dojha@sonwil.com>
2019-02-06 10:41:04 -0500

    changed main

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ app = Flask(__name__)
 def open_file():
     image_file = request.files['file']
     img_name= image_file.filename
-    image_file.save(os.path.join(app.config['/uploads'], img_name))
+    UPLOAD_FOLDER= '/static'
+    image_file.save(os.path.join(app.config[UPLOAD_FOLDER], img_name))
     API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
     http = httplib2.Http()
 
