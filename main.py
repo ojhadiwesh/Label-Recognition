f3283cf7969e51793d84d56d3e1aa8af8f3c2c47
Diwesh Ojha <dojha@sonwil.com>
2019-02-06 16:47:38 -0500

    updates to main and app.yaml

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, request
 from flask import Flask, render_template, redirect, url_for, request
 from google.cloud import datastore
 from google.cloud import storage
 from google.cloud import vision
-
-
+from google.cloud.vision import types
+import io
+from PIL import Image, ImageDraw
+from enum import Enum
 import os
 import logging
 from datetime import datetime
 CLOUD_STORAGE_BUCKET = os.environ.get('CLOUD_STORAGE_BUCKET')
 app = Flask(__name__)
+class FeatureType(Enum):
+    PAGE = 1
+    BLOCK = 2
+    PARA = 3
+    WORD = 4
+    SYMBOL = 5
 
 @app.route('/scan', methods=['POST'])
 def open_file():
@@ -38,73 +45,74 @@ def open_file():
     document= response.full_text_annotation
 
 
-    #
-    # def draw_boxes(image, bounds, color,width=5):
-    #     draw = ImageDraw.Draw(image)
-    #     for bound in bounds:
-    #         draw.line([
-    #         bound.vertices[0].x, bound.vertices[0].y,
-    #         bound.vertices[1].x, bound.vertices[1].y,
-    #         bound.vertices[2].x, bound.vertices[2].y,
-    #         bound.vertices[3].x, bound.vertices[3].y,
-    #         bound.vertices[0].x, bound.vertices[0].y],fill=color, width=width)
-    #     return image
-    # def get_document_bounds(response, feature):
-    #     for i,page in enumerate(document.pages):
-    #         for block in page.blocks:
-    #             if feature=='FeatureType.BLOCK':
-    #                 bounds.append(block.bounding_box)
-    #             for paragraph in block.paragraphs:
-    #                 if feature=='FeatureType.PARA':
-    #                     bounds.append(paragraph.bounding_box)
-    #                 for word in paragraph.words:
-    #                     for symbol in word.symbols:
-    #                         if (feature == 'FeatureType.SYMBOL'):
-    #                             bounds.append(symbol.bounding_box)
-    #                     if (feature == 'FeatureType.WORD'):
-    #                         bounds.append(word.bounding_box)
-    #     return bounds
-    # bounds=get_document_bounds(response, 'FeatureType.WORD')
-    # image= draw_boxes(image,bounds, 'yellow')
-    # def assemble_word(word):
-    #     assembled_word=""
-    #     for symbol in word.symbols:
-    #         assembled_word+=symbol.text
-    #     return assembled_word
-    # def find_word_location(document,word_to_find):
-    #     for page in document.pages:
-    #         for block in page.blocks:
-    #             for paragraph in block.paragraphs:
-    #                 for word in paragraph.words:
-    #                     assembled_word=assemble_word(word)
-    #                     if(assembled_word==word_to_find):
-    #                         return word.bounding_box
-    #
-    # location=find_word_location(document,'TRACKING')
-    # def text_within(document,x1,y1,x2,y2):
-    #     text=""
-    #     for page in document.pages:
-    #         for block in page.blocks:
-    #             for paragraph in block.paragraphs:
-    #                 for word in paragraph.words:
-    #                     for symbol in word.symbols:
-    #                         min_x=min(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
-    #                         max_x=max(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
-    #                         min_y=min(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
-    #                         max_y=max(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
-    #                         if(min_x >= x1 and max_x <= x2 and min_y >= y1 and max_y <= y2):
-    #                             text+=symbol.text
-    #                             if(symbol.property.detected_break.type==1 or
-    #                             symbol.property.detected_break.type==3):
-    #                                 text+=' '
-    #                             if(symbol.property.detected_break.type==2):
-    #                                 text+='\t'
-    #                             if(symbol.property.detected_break.type==5):
-    #                                 text+='\n'
-    #     return text
-    #
-    # tracking_id =text_within(document, location.vertices[1].x, location.vertices[1].y, 30+location.vertices[1].x+(location.vertices[1].x-location.vertices[0].x),location.vertices[2].y)
-    # # Create a Cloud Datastore client.
+
+    def draw_boxes(image, bounds, color,width=5):
+        draw = ImageDraw.Draw(image)
+        for bound in bounds:
+            draw.line([
+            bound.vertices[0].x, bound.vertices[0].y,
+            bound.vertices[1].x, bound.vertices[1].y,
+            bound.vertices[2].x, bound.vertices[2].y,
+            bound.vertices[3].x, bound.vertices[3].y,
+            bound.vertices[0].x, bound.vertices[0].y],fill=color, width=width)
+        return image
+    def get_document_bounds(response, feature):
+        bounds=[]
+        for i,page in enumerate(document.pages):
+            for block in page.blocks:
+                if feature=='FeatureType.BLOCK':
+                    bounds.append(block.bounding_box)
+                for paragraph in block.paragraphs:
+                    if feature=='FeatureType.PARA':
+                        bounds.append(paragraph.bounding_box)
+                    for word in paragraph.words:
+                        for symbol in word.symbols:
+                            if (feature == 'FeatureType.SYMBOL'):
+                                bounds.append(symbol.bounding_box)
+                        if (feature == 'FeatureType.WORD'):
+                            bounds.append(word.bounding_box)
+        return bounds
+    bounds=get_document_bounds(response, 'FeatureType.WORD')
+    image= draw_boxes(image,bounds, 'yellow')
+    def assemble_word(word):
+        assembled_word=""
+        for symbol in word.symbols:
+            assembled_word+=symbol.text
+        return assembled_word
+    def find_word_location(document,word_to_find):
+        for page in document.pages:
+            for block in page.blocks:
+                for paragraph in block.paragraphs:
+                    for word in paragraph.words:
+                        assembled_word=assemble_word(word)
+                        if(assembled_word==word_to_find):
+                            return word.bounding_box
+
+    location=find_word_location(document,'TRACKING')
+    def text_within(document,x1,y1,x2,y2):
+        text=""
+        for page in document.pages:
+            for block in page.blocks:
+                for paragraph in block.paragraphs:
+                    for word in paragraph.words:
+                        for symbol in word.symbols:
+                            min_x=min(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
+                            max_x=max(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
+                            min_y=min(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
+                            max_y=max(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
+                            if(min_x >= x1 and max_x <= x2 and min_y >= y1 and max_y <= y2):
+                                text+=symbol.text
+                                if(symbol.property.detected_break.type==1 or
+                                symbol.property.detected_break.type==3):
+                                    text+=' '
+                                if(symbol.property.detected_break.type==2):
+                                    text+='\t'
+                                if(symbol.property.detected_break.type==5):
+                                    text+='\n'
+        return text
+
+    tracking_id =text_within(document, location.vertices[1].x, location.vertices[1].y, 30+location.vertices[1].x+(location.vertices[1].x-location.vertices[0].x),location.vertices[2].y)
+    # Create a Cloud Datastore client.
     datastore_client = datastore.Client()
 
     # Fetch the current date / time.
@@ -124,7 +132,7 @@ def open_file():
     entity['blob_name'] = blob.name
     entity['image_public_url'] = blob.public_url
     entity['timestamp'] = current_datetime
-    # entity['text'] = tracking_id
+    entity['tracking'] = tracking_id
     # Save the new entity to Datastore.
     datastore_client.put(entity)
-    return document
+    return tracking_id
