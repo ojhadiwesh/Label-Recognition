63988cfacd2e6ef07235ed3e1a6d347eb239a5d1
Diwesh Ojha <dojha@sonwil.com>
2019-02-07 14:50:52 -0500

    by para

--- main.py
+++ main.py
@@ -42,7 +42,12 @@ def open_file():
     image = vision.types.Image(
         source=vision.types.ImageSource(gcs_image_uri=source_uri))
     response = vision_client.text_detection(image)
-    document= response.full_text_annotation
+    annotations= response.text_annotations
+    if len(annotations)>0:
+        text= annotations[0].description
+    else:
+        text=''
+
 
 
     #
@@ -56,64 +61,67 @@ def open_file():
     #         bound.vertices[3].x, bound.vertices[3].y,
     #         bound.vertices[0].x, bound.vertices[0].y],fill=color, width=width)
     #     return image
-    def get_document_bounds(response, feature):
-        bounds=[]
-        for i,page in enumerate(document.pages):
-            for block in page.blocks:
-                if feature==FeatureType.BLOCK:
-                    bounds.append(block.bounding_box)
-                for paragraph in block.paragraphs:
-                    if feature==FeatureType.PARA:
-                        bounds.append(paragraph.bounding_box)
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            if (feature == FeatureType.SYMBOL):
-                                bounds.append(symbol.bounding_box)
-                        if (feature == FeatureType.WORD):
-                            bounds.append(word.bounding_box)
-        return bounds
-    bounds=get_document_bounds(response, FeatureType.WORD)
-    # image= draw_boxes(image,bounds, 'yellow')
-    def assemble_word(word):
-        assembled_word=""
-        for symbol in word.symbols:
-            assembled_word+=symbol.text
-        return assembled_word
-    def find_word_location(document,word_to_find):
-        for page in document.pages:
-            for block in page.blocks:
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        assembled_word=assemble_word(word)
-                        if(assembled_word==word_to_find):
-                            return word.bounding_box
-
-
-    location=find_word_location(document,'Ship')
-    def text_within(document,x1,y1,x2,y2):
-        text=""
-        for page in document.pages:
-            for block in page.blocks:
-                for paragraph in block.paragraphs:
-                    for word in paragraph.words:
-                        for symbol in word.symbols:
-                            min_x=min(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
-                            max_x=max(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
-                            min_y=min(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
-                            max_y=max(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
-                            if(min_x >= x1 and max_x <= x2 and min_y >= y1 and max_y <= y2):
-                                text+=symbol.text
-                                if(symbol.property.detected_break.type==1 or
-                                symbol.property.detected_break.type==3):
-                                    text+=' '
-                                if(symbol.property.detected_break.type==2):
-                                    text+='\t'
-                                if(symbol.property.detected_break.type==5):
-                                    text+='\n'
-        return text
-
-    ship_to =text_within(document, location.vertices[1].x, location.vertices[1].y, 30+location.vertices[1].x+(location.vertices[1].x-location.vertices[0].x),location.vertices[2].y)
-    # Create a Cloud Datastore client.
+    # def get_document_bounds(response, feature):
+    #     bounds=[]
+    #     for i,page in enumerate(document.pages):
+    #         for block in page.blocks:
+    #             if feature==FeatureType.BLOCK:
+    #                 bounds.append(block.bounding_box)
+    #             for paragraph in block.paragraphs:
+    #                 if feature==FeatureType.PARA:
+    #                     bounds.append(paragraph.bounding_box)
+    #                 for word in paragraph.words:
+    #                     for symbol in word.symbols:
+    #                         if (feature == FeatureType.SYMBOL):
+    #                             bounds.append(symbol.bounding_box)
+    #                     if (feature == FeatureType.WORD):
+    #                         bounds.append(word.bounding_box)
+    #     return bounds
+    # bounds=get_document_bounds(response, FeatureType.WORD)
+    # # image= draw_boxes(image,bounds, 'yellow')
+    # def assemble_word(word):
+    #     assembled_word=""
+    #     for symbol in word.symbols:
+    #         assembled_word+=symbol.text
+    #     return assembled_word
+    # def find_word_location(document,word_to_find):
+    #     for page in document.pages:
+    #         for block in page.blocks:
+    #             for paragraph in block.paragraphs:
+    #                 for word in paragraph.words:
+    #                     assembled_word=assemble_word(word)
+    #                     if(assembled_word==word_to_find):
+    #                         return word.bounding_box
+    #
+    # def find_block_location(word):
+    #     for page in document.pages:
+    #         for block in page.blocks:
+    #             for paragraph in
+    # location=find_word_location(document,'Ship')
+    # def text_within(document,x1,y1,x2,y2):s
+    #     text=""
+    #     for page in document.pages:
+    #         for block in page.blocks:
+    #             for paragraph in block.paragraphs:
+    #                 for word in paragraph.words:
+    #                     for symbol in word.symbols:
+    #                         min_x=min(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
+    #                         max_x=max(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
+    #                         min_y=min(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
+    #                         max_y=max(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
+    #                         if(min_x >= x1 and max_x <= x2 and min_y >= y1 and max_y <= y2):
+    #                             text+=symbol.text
+    #                             if(symbol.property.detected_break.type==1 or
+    #                             symbol.property.detected_break.type==3):
+    #                                 text+=' '
+    #                             if(symbol.property.detected_break.type==2):
+    #                                 text+='\t'
+    #                             if(symbol.property.detected_break.type==5):
+    #                                 text+='\n'
+    #     return text
+    #
+    # ship_to =text_within(document, location.vertices[1].x, location.vertices[1].y, 30+location.vertices[1].x+(location.vertices[1].x-location.vertices[0].x),location.vertices[2].y)
+    # # Create a Cloud Datastore client.
     datastore_client = datastore.Client()
 
     # Fetch the current date / time.
@@ -133,7 +141,7 @@ def open_file():
     entity['blob_name'] = blob.name
     entity['image_public_url'] = blob.public_url
     entity['timestamp'] = current_datetime
-    entity['ship_to'] = ship_to
+    entity['text'] = text
     # Save the new entity to Datastore.
     datastore_client.put(entity)
-    return ship_to
+    return text
